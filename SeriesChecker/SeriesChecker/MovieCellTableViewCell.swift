8d0cf21e649d30c8c930bcca5a4ec7f49ed84362
LAB CCET <labccet@Lab-mac-10.local>
2017-06-12 17:15:24 -0300

    ïniciando interface

---
+++ SeriesChecker/SeriesChecker/MovieCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCellTableViewCell.swift
+//  SeriesChecker
+//
+//  Created by LAB CCET on 12/06/17.
+//  Copyright © 2017 Patati. All rights reserved.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
